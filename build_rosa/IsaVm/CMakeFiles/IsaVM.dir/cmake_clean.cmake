ab14fb1f2f3ca8b62520a5352c8eaf7fee295c5e
NorthernHell <massabrutto@gmail.com>
2015-12-02 14:02:31 +0300

    builded nice :)

---
+++ build_rosa/IsaVm/CMakeFiles/IsaVM.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IsaVM.dir/dipl0lnk.c.o"
+  "CMakeFiles/IsaVM.dir/divm0mai.c.o"
+  "../Cmds/IsaVM.pdb"
+  "../Cmds/IsaVM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IsaVM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
